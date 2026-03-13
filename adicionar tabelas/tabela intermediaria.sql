d36a2f79bf3038ac8c9f455ba685b1240ea5dffa
viniciusclima12-pixel <vinicius.c.lima12@aluno.senai.br>
2026-03-13 16:31:45 -0300

    Delete adicionar tabelas directory

--- adicionar tabelas/tabela intermediaria.sql
+++
@@ -1,8 +0,0 @@
--- Active: 1773421575078@@127.0.0.1@3306
-CREATE TABLE mercado_dono(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    mercado_id INTEGER,
-    dono_id INTEGER,
-    FOREIGN KEY (mercado_id) REFERENCES mercado (id),
-    FOREIGN KEY (dono_id) REFERENCES dono (id)
-);
